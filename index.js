1b941562784c77ff5590f383ccfd0b90ebd788fc
CoteQuentin <101783057+CoteQuentin@users.noreply.github.com>
2022-07-25 20:31:54 -0400

    Update index.js
    Remove function for cycling hero images.

--- index.js
+++ index.js
@@ -6,48 +6,7 @@ function fadeOut(element) {
     element.classList.add("fade");
 }
 
-function setImg() {
-    let heroImg = document.querySelector(".hero-img");
-    let currentImg = heroImg.getAttribute("src");
-    let hiddenHero = document.querySelector(".hidden-hero");
-
-    fadeIn(heroImg);
-
-    setTimeout(function() {
-        fadeOut(heroImg);
-
-    setTimeout(function() {
-        if (currentImg === "images/hero-image2.jpg") {
-            heroImg.setAttribute("src", "images/hero-image3.jpg");
-        } else {
-            heroImg.setAttribute("src", "images/hero-image2.jpg");
-        }
-        
-        setTimeout(function() {
-            fadeIn(heroImg);
-            // heroImg.classList.remove("fade");
-            
-            setTimeout(function() {
-                fadeOut(heroImg);
-                // heroImg.classList.add("fade");
-
-                setTimeout(function() {
-                    heroImg.style.visibility = "hidden";
-                    fadeIn(hiddenHero);
-                    // hiddenHero.classList.remove("fade");
-
-                    setTimeout(function() {
-                        return
-                    }, 800);
-                }, 800);
-            }, 3000);
-        }, 100);
-    }, 800);
-    }, 3000);
-}
-setTimeout(setImg, 250);
-
-let navBtn = document.querySelector(".collapsible");
+let navBtn = document.querySelector(".nav-btn");
 let navList = document.querySelector(".navbar-list");
 
 navBtn.addEventListener("click", function() {
